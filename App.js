2c3dc6b0bb79e210fc5a70575ef6bf16322e1d3d
Jessim <jessim.heddadi@influence4you.com>
2023-01-23 15:05:18 +0100

    import NavigationContainer and StackNavigator to navigate in app

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
+// WRAP ALL APP TO NAVIGATE
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light" />
-      <SafeAreaView>
-        <CategoriesScreen />
-      </SafeAreaView>
+      <StatusBar style="dark" />
+
+      {/* WRAP ALL APP TO NAVIGATE */}
+
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
     </>
   );
 }
